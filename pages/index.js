dd41e9668f555cade4bb6103cfc6a2811bdd13f5
thenacholibre <thenacholibre@users.noreply.github.com>
2020-12-01 17:46:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello, World!</h1>
+        <h1 className={styles.title}>Hello, World 123!</h1>
       </main>
     </div>
   );
